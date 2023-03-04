648a761afd5fd6ed184a49ba15cacd48413ca47a
Guilherme Ferreira <guilherme.ea16013@gmail.com>
2023-03-04 08:42:08 -0300

    Fazendo upload de arquivos

---
+++ Aula1/src/Conta.java
@@ -0,0 +1,9 @@
+public class Conta {
+
+     String titular;
+    private  Double saldo;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+}
---
+++ Aula1/src/Gato.java
@@ -0,0 +1,12 @@
+public class Gato {
+    String cor = "Preto";
+    int tamanho;
+    String genero;
+
+
+    public void miar() {
+        System.out.println("miau");
+    }
+
+
+}
